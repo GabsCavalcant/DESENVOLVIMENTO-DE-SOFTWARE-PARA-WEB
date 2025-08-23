98354834650a5d4ef2a6e0e9cd42e49766438012
GabsCavalcant <gabrielnina22@gmail.com>
2025-08-23 17:07:58 -0300

    Exercicios do cap 1

---
+++ “OlaMundoWeb1”/src/java/olamundoweb/servlets/OlaServlet.java
@@ -0,0 +1,34 @@
+package olamundoweb.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author gabri
+ */
+@WebServlet(name = "OlaServlet", urlPatterns = {"/ola"})
+public class OlaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) {
+
+        System.out.println("Ola Mundo!");
+        System.out.println("Meu Primeiro Servlet!");
+
+    }
+
+}
